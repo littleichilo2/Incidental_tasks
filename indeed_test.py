2499050940edfb00d08fc224c6a018f5ea9b75ea
littleichilo2 <32761792+littleichilo2@users.noreply.github.com>
2018-09-09 23:05:36 +0900

    Update indeed_test.py

--- indeed_test.py
+++ indeed_test.py
@@ -58,21 +58,14 @@ while(endflag==0):
             page=driver.find_elements_by_xpath('//table[@id="hjhome"]/tbody/tr[@class="datarow unique"]/td[@class="left first"]/a')
             pagename=lxml.html.fromstring(driver.page_source).xpath('//table[@id="hjhome"]/tbody/tr[@class="datarow unique"]/td[@class="left first"]/a/text()')[k].replace('株式会社リクルーティング・デザイン for  ','').replace('株式会社リクルーティング・デザイン for ','').replace('株式会社リクルーティング・デザイン for　','').replace('株式会社リクルーティング・デザイン for','').replace('株式会社リクルーティング・デザイン foe ','')
             page[k].click()
-            wait.until(ec.presence_of_all_elements_located)
             if lxml.html.fromstring(driver.page_source).xpath("/html/body/div[@id='page_frame']/div[@id='page_content_wrapper']/div[@id='page_content']/div[@class='page_indented_content']/div[@id='result_options']/div[@class='result_option'][1]/p")==[]:
                 driver.get("https://ads.indeed.com/job/ads?startDate="+startdate.strftime("%Y-%m-01")+"&endDate="+startdate.strftime("%Y-%m")+'-'+str(monthrange(startdate.year,startdate.month)[1]))
                 time.sleep(3)
             #日別CSVダウンロード
-                try:
-                    driver.find_element_by_xpath('//div[@id="table_header"]/ul[@class="report_tab"]/li[@class="unselected"]/a').click()
-                except:
-                    pass
-                wait.until(ec.presence_of_all_elements_located)
-                try:
-                    driver.find_element_by_xpath('//div[@id="filter_options"]/a[@class="margin-right"]').click()
-                except:
-                    pass
-                wait.until(ec.presence_of_all_elements_located)
+                driver.find_element_by_xpath('//div[@id="table_header"]/ul[@class="report_tab"]/li[@class="unselected"]/a').click()
+                time.sleep(3)
+                driver.find_element_by_xpath('//div[@id="filter_options"]/a[@class="margin-right"]').click()
+                time.sleep(3)
                 os.chdir("C:\\Users\\seisaku\\Desktop\\Indeed\\buffer_2")
                 for f in listdir("C:\\Users\\seisaku\\Desktop\\Indeed\\buffer_2"):
                     df= pd.read_csv(f,engine='python')
@@ -88,22 +81,15 @@ while(endflag==0):
                     driver.find_element_by_tag_name("body").send_keys(Keys.F5)
                     print("F5")
                     time.sleep(10)
-                try:
-                    wait.until(ec.element_to_be_clickable((By.XPATH, "//div[@id='body-container']/div[@class='ia-BodyWrapper']/div[@class='ia-BodyMain']/div/div[@class='icl-Grid'][3]/div[@class='icl-Grid-col icl-u-xs-span10']/div/div[@class='ia-PerfReportToolBar']/div[@class='ia-PerfReportToolBarItem'][3]/button[@class='icl-Button--secondary icl-Button--sm ia-PerfReportToolBarItem-dropbtn']")))
                     webdriver.ActionChains(driver).move_to_element(driver.find_element_by_xpath("//div[@id='body-container']/div[@class='ia-BodyWrapper']/div[@class='ia-BodyMain']/div/div[@class='icl-Grid'][3]/div[@class='icl-Grid-col icl-u-xs-span10']/div/div[@class='ia-PerfReportToolBar']/div[@class='ia-PerfReportToolBarItem'][3]/button[@class='icl-Button--secondary icl-Button--sm ia-PerfReportToolBarItem-dropbtn']")).perform()
-                    wait.until(ec.element_to_be_clickable((By.XPATH,"//div[@id='body-container']/div[@class='ia-BodyWrapper']/div[@class='ia-BodyMain']/div/div[@class='icl-Grid'][3]/div[@class='icl-Grid-col icl-u-xs-span10']/div/div[@class='ia-PerfReportToolBar']/div[@class='ia-PerfReportToolBarItem'][3]/div[@class='ia-PerfReportToolBarItem-content']/a[2]")))
+                    time.sleep(3)
                     webdriver.ActionChains(driver).click(driver.find_element_by_xpath("//div[@id='body-container']/div[@class='ia-BodyWrapper']/div[@class='ia-BodyMain']/div/div[@class='icl-Grid'][3]/div[@class='icl-Grid-col icl-u-xs-span10']/div/div[@class='ia-PerfReportToolBar']/div[@class='ia-PerfReportToolBarItem'][3]/div[@class='ia-PerfReportToolBarItem-content']/a[2]")).perform()
-                    wait.until(ec.presence_of_all_elements_located)
                     time.sleep(5)
-                except:
-                    driver.find_element_by_tag_name("body").send_keys(Keys.F5)
-                    print("F5")
-                    wait.until(ec.element_to_be_clickable((By.XPATH, "//div[@id='body-container']/div[@class='ia-BodyWrapper']/div[@class='ia-BodyMain']/div/div[@class='icl-Grid'][3]/div[@class='icl-Grid-col icl-u-xs-span10']/div/div[@class='ia-PerfReportToolBar']/div[@class='ia-PerfReportToolBarItem'][3]/button[@class='icl-Button--secondary icl-Button--sm ia-PerfReportToolBarItem-dropbtn']")))
+                else:
                     webdriver.ActionChains(driver).move_to_element(driver.find_element_by_xpath("//div[@id='body-container']/div[@class='ia-BodyWrapper']/div[@class='ia-BodyMain']/div/div[@class='icl-Grid'][3]/div[@class='icl-Grid-col icl-u-xs-span10']/div/div[@class='ia-PerfReportToolBar']/div[@class='ia-PerfReportToolBarItem'][3]/button[@class='icl-Button--secondary icl-Button--sm ia-PerfReportToolBarItem-dropbtn']")).perform()
-                    wait.until(ec.element_to_be_clickable((By.XPATH,"//div[@id='body-container']/div[@class='ia-BodyWrapper']/div[@class='ia-BodyMain']/div/div[@class='icl-Grid'][3]/div[@class='icl-Grid-col icl-u-xs-span10']/div/div[@class='ia-PerfReportToolBar']/div[@class='ia-PerfReportToolBarItem'][3]/div[@class='ia-PerfReportToolBarItem-content']/a[2]")))
+                    time.sleep(3)
                     webdriver.ActionChains(driver).click(driver.find_element_by_xpath("//div[@id='body-container']/div[@class='ia-BodyWrapper']/div[@class='ia-BodyMain']/div/div[@class='icl-Grid'][3]/div[@class='icl-Grid-col icl-u-xs-span10']/div/div[@class='ia-PerfReportToolBar']/div[@class='ia-PerfReportToolBarItem'][3]/div[@class='ia-PerfReportToolBarItem-content']/a[2]")).perform()
-                    wait.until(ec.presence_of_all_elements_located)
-                time.sleep(5)
+                    time.sleep(5)
 
                 os.chdir("C:\\Users\\seisaku\\Desktop\\Indeed\\buffer_2")
                 for f in listdir("C:\\Users\\seisaku\\Desktop\\Indeed\\buffer_2"):
@@ -119,6 +105,7 @@ while(endflag==0):
             time.sleep(5)
             if k == len(pagelist)-1:
                 endflag=1
+                break
     except:
         print(buffff)
         os.chdir("C:\\Users\\seisaku\\Desktop\\Indeed\\buffer_2")
@@ -126,6 +113,15 @@ while(endflag==0):
             os.rename(f,'1.csv')
             os.remove('C:\\Users\\seisaku\\Desktop\\Indeed\\buffer_2\\1.csv')
         startflag=buffff
+        driver.close()
+        driver.get("https://secure.indeed.com/account/login?service=mob&hl=ja_JP&co=JP&continue=https%3A%2F%2Fjp.indeed.com%2F%3F_ga%3D2.167660222.1622849172.1514262980-517429143.1514262980&tmpl=dmobile")
+        wait.until(ec.presence_of_all_elements_located)
+        driver.find_element_by_id('signin_email').clear()
+        driver.find_element_by_id('signin_email').send_keys("")
+        driver.find_element_by_id('signin_password').clear()
+        driver.find_element_by_id('signin_password').send_keys("")
+        driver.find_element_by_xpath('//form[@id="loginform"]/button[@class="icl-Button icl-Button--primary icl-Button--md icl-Button--block"]').click()
+        wait.until(ec.presence_of_all_elements_located)
 driver.quit()
 os.chdir("C:\\Users\\seisaku\\Desktop\\Indeed\\monthly\\daily")
 excel_writer = pd.ExcelWriter('alldata_daily.xlsx')
