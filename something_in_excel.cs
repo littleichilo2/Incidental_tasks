52cf9b90ed5a4df563e7a976686777e13b12db06
littleichilo2 <32761792+littleichilo2@users.noreply.github.com>
2019-02-26 13:39:19 +0900

    Update something_in_excel.cs

--- something_in_excel.cs
+++ something_in_excel.cs
@@ -1,9 +1,32 @@
 //現在のパスを引き出せる
 System.IO.Path.GetDirectoryName(Application.ExecutablePath);
 
+
+//Rangeでサーチする
+Excel.Range colRange = Worksheets.Columns["A:Z"]
+Excel.Range resultRange = colRange.Find(
+  What: searchString,
+  LookIn: Excel.XlFindLookIn.xlValues,
+  LookAt: Excel.XlLookAt.xlPart,
+  SearchOrder: Excel.XlSearchOrder.xlByRows,
+  SearchDirection: Excel.XlSearchDirection.xlNext
+);
+if (resultRange is null){
+  //
+}
+else
+{
+  //then handle
+}
+xlWorkBook.CLose(false);
+xlApp.Quit();
+releaseObject(xlWorkSheet);
+releaseObject(xlWorkBook);
+releaseObject(xlApp);
+
 //Excel対象番号をサーチする
 var keyword = number;
-var hitcell = Excel.Wroksheets("Sheet1").Cells.Find(number);
+var hitcell = Excel.Worksheets("Sheet1").Cells.Find(number);
 var message = "";
 if (hitRange == null){
   //insert new object
