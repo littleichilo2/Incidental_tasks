9e338ef3e543bb94a891cfddc6153dbe02314813
littleichilo2 <32761792+littleichilo2@users.noreply.github.com>
2019-03-29 14:58:50 +0900

    Update originalcsharp.cs

--- originalcsharp.cs
+++ originalcsharp.cs
@@ -24,9 +24,21 @@ namespace Listening_comprehension
         string gender;
         TimeSpan totaltime = TimeSpan.FromMilliseconds(0);
         List<int> titlelist = new List<int>(){
-            1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,
-            30,31,32,33,34,35,36,37,38,39,40
+            1,4,9,14,18,21,25,29,33,37
         };
+        List<int> title_1 = new List<int>(){
+          2,5,10,15,19,22,26,30,34,38
+        };
+        List<int> title_2 = new List<int>(){
+          3,6,11,16,20,23,27,31,35,39
+        };
+        List<int> title_3 = new List<int>(){
+          7,12,17,24,28,32,36,40
+        };
+        List<int> title_4 = new List<int>(){
+          8,13
+        };
+        
         List<int> list_1 = new List<int>() { 2, 3, 5, 6, 7, 8, 10, 11, 12, 13, 15, 16, 17, 19, 20, 22, 23, 24, 26, 27, 28, 30, 31, 32, 34, 35, 36, 38, 39, 40 };
         List<int> list_2 = new List<int>() { 2, 3, 5, 6, 7, 8, 10, 11, 12, 13, 15, 16, 17, 19, 20, 22, 23, 24, 26, 27, 28, 30, 31, 32, 34, 35, 36, 38, 39, 40 };
         List<int> list_3 = new List<int>() { 2, 3, 5, 6, 7, 8, 10, 11, 12, 13, 15, 16, 17, 19, 20, 22, 23, 24, 26, 27, 28, 30, 31, 32, 34, 35, 36, 38, 39, 40 };
@@ -221,6 +233,27 @@ namespace Listening_comprehension
                 nextbutton.Text = "開始答題";
                 nextbutton.Visible = true;
             }
+            if (title_1.IndexOf(frame) != -1)
+            {
+                titlelabel.Text = "第一題";
+                titlelabel.Visible = true;
+            }
+            if (title_2.IndexOf(frame) != -1)
+            {
+                titlelabel.Text = "第二題";
+                titlelabel.Visible = true;
+            }
+            if (title_3.IndexOf(frame) != -1)
+            {
+                titlelabel.Text = "第三題";
+                titlelabel.Visible = true;
+            }
+            if (title_4.IndexOf(frame) != -1)
+            {
+                titlelabel.Text = "第四題";
+                titlelabel.Visible = true;
+            }
+            
             if (frame == 41)
             {
                 endbutton.Visible = true;
